6ce18653e28ccb788b23cc3283239bddad7bda79
Simon Krauter <trustablecode@gmail.com>
2020-03-25 10:01:45 +0100

    Move country info to extra file

--- main.js
+++ main.js
@@ -1,53 +1,5 @@
 "use strict";
 
-const population =
-{ "China": 1427647786
-, "Italy": 60317546
-, "Iran": 83183741
-, "Spain": 46733038
-, "Germany": 83149300
-, "France": 67022000
-, "United States": 328239523
-, "South Korea": 51709098
-, "United Kingdom": 67545757
-, "Netherlands": 17424978
-, "Japan": 126150000
-, "Switzerland": 8570146
-, "Indonesia": 267670543
-, "Philippines": 100981437
-, "Austria": 8902600
-, "Norway": 5367580
-, "Belgium": 11515793
-, "Sweden": 10333456
-, "Denmark": 5822763
-, "Iraq": 38433600
-, "Poland": 38386000
-, "Malaysia": 32772100
-, "Canada": 37894799
-, "Portugal": 10276617
-, "San Marino": 33344
-, "Algeria": 43000000
-, "Australia": 25652000
-, "Brazil": 210147125
-, "Greece": 10768477
-, "India": 1352642280
-, "Egypt": 100075480
-, "Turkey": 82003882
-, "Luxembourg": 613894
-, "Peru": 32824358
-, "Ecuador": 17084358
-, "Lebanon": 6859408
-, "Argentina": 44938712
-, "Hungary": 9772756
-};
-
-const countriesWikiUrlThe =
-[ "United States"
-, "United Kingdom"
-, "Netherlands"
-, "Philippines"
-];
-
 const casesIncreaseDays = 4;
 
 var countries = {};
@@ -88,7 +40,7 @@ function onLoadDataFinished() {
   , "Korea, South": "South Korea"
   };
   const invalidCountries =
-  [ "Cruise Ship"
+  [ "Diamond Princess"
   ];
   const data = JSON.parse(this.responseText);
   countries = [];
